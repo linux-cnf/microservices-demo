2b81ae776775c737e468abc53cf6bcd4e38ac9f1
Bablish Jaiwal <linux.cnf@gmail.com>
2026-05-26 04:30:40 +0000

    chore: rename argocd external secrets workload identity ksa

--- terraform/live/main/external-secrets-iam.tf
+++ terraform/live/main/external-secrets-iam.tf
@@ -1,7 +1,7 @@
 resource "google_service_account_iam_member" "external_secrets_argocd_workload_identity" {
   service_account_id = "projects/project-9e0b2bd9-4649-487c-9d1/serviceAccounts/external-secrets-gsm@project-9e0b2bd9-4649-487c-9d1.iam.gserviceaccount.com"
   role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:project-9e0b2bd9-4649-487c-9d1.svc.id.goog[argocd/external-secrets]"
+  member             = "serviceAccount:project-9e0b2bd9-4649-487c-9d1.svc.id.goog[argocd/argocd-external-secrets]"
 }
 
 resource "google_project_iam_member" "external_secrets_secret_accessor" {
